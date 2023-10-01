1582b50a64f767380fe82dbbff984a23f8ad07f1
void-zenith <zenithrajbhandari@gmail.com>
2023-09-30 23:03:55 -0400

    api for posting new products

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const HOST = "127.0.0.1";
 let errors = require("restify-errors");
 let restify = require("restify");
 
-let productsSave = require("save")("products");
+let productData = require("save")("products");
 // Create the restify server
 let server = restify.createServer({ name: SERVER });
 
@@ -49,4 +49,46 @@ server.use(function (req, res, next) {
     console.log(`${req.route.path} ${req.method}: sending response`);
   });
   next();
+});
+
+// Sample product data (initially empty)
+let productList = [];
+
+// POST Request -> for creating new products
+server.post("/products", function (req, res, next) {
+  // Validation of mandatory fields
+  if (
+    req.body.name === undefined ||
+    req.body.price === undefined ||
+    req.body.quantity === undefined
+  ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(
+      new errors.BadRequestError("Name, Price, and Quantity must be inserted")
+    );
+  }
+
+  let newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+    quantity: req.body.quantity,
+  };
+
+  productData.create(newProduct, function (error, product) {
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new Error(JSON.stringify(error.errors)));
+
+    // Send the product if no issues
+    res.send(201, product);
+  });
+});
+
+// GET -> for getting all the products
+server.get("/products", function (req, res, next) {
+  console.log("GET /products params=>" + JSON.stringify(req.params));
+
+  productData.find({}, function (error, products) {
+    // Return all of the products in the system
+    res.send(products);
+  });
 });
